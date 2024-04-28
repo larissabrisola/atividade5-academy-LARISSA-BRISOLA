b1996601ceb74c0d5a6cdcc7f1ad3522a4ad9568
larissabrisola <alarissabrisola@gmail.com>
2024-04-28 17:55:59 -0300

    searchUser ok

--- cypress/e2e/listUsers.cy.js
+++ cypress/e2e/listUsers.cy.js
@@ -4,6 +4,27 @@ describe("Listar usuários", () => {
   });
 
   it("Listar todos usuários cadastrados", () => {
+    cy.intercept('GET', '', {
+      statusCode: 200, 
+      body: [
+        {
+          "id": "8fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "name": "Joana",
+          "email": "user@example.com",
+          "createdAt": "2024-04-28T20:43:51.446Z",
+          "updatedAt": "2024-04-28T20:43:51.446Z"
+        }, 
+        {
+          "id": "9fa85f64-5717-4562-b3fc-2c963f66af86",
+          "name": "Pedro",
+          "email": "user@example.com",
+          "createdAt": "2024-04-28T20:43:51.446Z",
+          "updatedAt": "2024-04-28T20:43:51.446Z"
+        }
+      ]
+    })
+
+
     cy.get("#listaUsuarios").should("be.visible");
   });
 
@@ -14,8 +35,6 @@ describe("Listar usuários", () => {
     }).as("emptyList");
 
     cy.wait("@emptyList");
-    // pegar o texto de naoe xiste usuario
-    // pegar botao de criar novo usuario
 
     cy.get("h3")
       .contains("Ops! Não existe nenhum usuário para ser exibido.")
